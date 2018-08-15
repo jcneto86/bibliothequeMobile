6f09af36fad00cebe206b18435db0cb90fadc1ea
João Carlos Fernandes Neto <wysrar@gmail.com>
2018-08-15 13:14:29 -0400

    v0.2  - Activity créées

---
+++ app/src/main/java/com/jcneto/bibliothequemobile/ChangerMotPasseActivity.java
@@ -0,0 +1,14 @@
+package com.jcneto.bibliothequemobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ChangerMotPasseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_changer_mot_passe);
+    }
+
+}
---
+++ app/src/main/java/com/jcneto/bibliothequemobile/FaireReservationRareActivity.java
@@ -0,0 +1,14 @@
+package com.jcneto.bibliothequemobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class FaireReservationRareActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faire_reservation_rare);
+    }
+
+}
