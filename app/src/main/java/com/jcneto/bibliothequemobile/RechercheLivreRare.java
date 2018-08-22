2e3e1b8fb4ad89eb197dfcfda48047f4177b7063
Brice <igbrice@hotmail.com>
2018-08-22 12:14:06 -0400

    v0.4 Nomer ID Batti Interface

---
+++ app/src/main/java/com/jcneto/bibliothequemobile/RechercheLivreRare.java
@@ -0,0 +1,13 @@
+package com.jcneto.bibliothequemobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RechercheLivreRare extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recherche_rares);
+    }
+}
